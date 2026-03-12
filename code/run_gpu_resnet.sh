0a0e065b72396c29ad74eb977839c69514322c3d
hilalyldz <yildizhi16@itu.edu.tr>
2026-03-12 09:57:19 +0100

    .sh files updated

---
+++ code/run_gpu_resnet.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=resnet
+#SBATCH --output=results/resnet_%A_%a.out
+#SBATCH --error=results/resnet_%A_%a.err
+#SBATCH --partition=hpda2_compute_gpu
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=32G
+#SBATCH --time=06:00:00
+
+source ~/.bashrc
+conda activate resnet312
+
+FEATURE=wavelet
+
+python -u run_training.py --dataset satellite --feature $FEATURE --gpu-id 0
+
---
+++ code/run_gpu_resnet_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=resnet
+#SBATCH --output=results/resnet_%A_%a.out
+#SBATCH --error=results/resnet_%A_%a.err
+#SBATCH --partition=hpda2_compute_gpu
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=32G
+#SBATCH --time=06:00:00
+
+source ~/.bashrc
+conda activate resnet312
+
+FEATURE=fft
+
+python -u run_test.py --dataset=CycleGAN --feature=fft
+
